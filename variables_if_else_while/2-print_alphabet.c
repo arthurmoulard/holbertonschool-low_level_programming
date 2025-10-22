c73b521efb52c44c3d94df37d89cf6b084e317a8
arthurmoulard <arthurmoulardperso@gmail.com>
2025-10-22 07:18:11 +0000

    task2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int main(void);
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+
+		alphabet++;
+	}
+}
+
+	return (0);
+}
