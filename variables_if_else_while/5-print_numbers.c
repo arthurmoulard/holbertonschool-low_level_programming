586334beb60106fb621d2d921817d16269bf657a
arthurmoulard <arthurmoulardperso@gmail.com>
2025-10-22 08:37:55 +0000

    task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num = '0'
+
+	while (num <= '9')
+	{
+		putchar(num);
+
+		num++;
+	}
+}
