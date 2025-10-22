0c60480e3dd2bef97ab9433f26b74b564e39ab18
arthurmoulard <arthurmoulardperso@gmail.com>
2025-10-22 07:29:20 +0000

    task2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/* betty style doc for function main goes there */
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int main(void);
