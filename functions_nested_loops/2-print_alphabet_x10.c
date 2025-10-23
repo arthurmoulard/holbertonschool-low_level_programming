bcfaf6f8b22ab128db223472222f222e772c2f9b
arthurmoulard <arthurmoulardperso@gmail.com>
2025-10-23 23:35:09 +0000

    task2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_alphabet_x10(void);
+{
+	char A;
+	char X;
+
+	for (X = '0'; X <= '9'; X++)
+	{
+		for (A = 'a'; A <= 'z'; A++)
+		{
+	_putchar(A);
+		}
+	_putchar('\n');
+	}
+}
