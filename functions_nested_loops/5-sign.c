b82efecd8c3aa9470e47c5adf235ed05d1d309ce
arthurmoulard <arthurmoulardperso@gmail.com>
2025-10-24 00:18:36 +0000

    task5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,22 +4,22 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-')
-		_putchar(',')
-		return(-1)
+		_putchar('-');
+		_putchar(',');
+		return(-1);
 	}
 
 	if (n > 0)
 	{
-		_putchar('+')
-		_putchar(',')
-		return(1)
+		_putchar('+');
+		_putchar(',');
+		return(1);
 	}
 
 	else
 	{
-		_putchar('0')
-		_putchar(',')
-		return(0)
+		_putchar('0');
+		_putchar(',');
+		return(0);
 	}
 }
