601797b6e9e1cac38a88c68946fb3dd4f417eb6a
arthurmoulard <arthurmoulardperso@gmail.com>
2025-10-27 13:43:44 +0000

    task4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,9 +2,11 @@
 
 void print_most_numbers(void)
 {
-int c
-	for(c = '0'; c <= '9'; c++)
-		(c != 2; 4)
-			_putchar(c)
-			_putchar('\n')
+	int c;
+	for(c = '0' && c <= '9'; c++)
+	{
+		if(c != '2' && c != '4')
+		_putchar(c);
+	}
+		_putchar('\n');
 }
