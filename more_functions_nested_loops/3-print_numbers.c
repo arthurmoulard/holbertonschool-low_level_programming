530db04a2d518de592aee4c2bd2b4d30638c006b
arthurmoulard <arthurmoulardperso@gmail.com>
2025-10-27 12:52:14 +0000

    task3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,11 @@
 
 void print_numbers(void)
 {
-	char i;
+        char c;
 
-	for (i >= '0' && i <= '9'; i++);
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+        for (c = '0'; c <= '9'; c++)
+        {
+                _putchar(c);
+        }
+        _putchar('\n');
 }
