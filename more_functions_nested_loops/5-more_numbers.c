14cac15d107de163c6066714a71add0c55aba7d6
arthurmoulard <arthurmoulardperso@gmail.com>
2025-10-28 09:06:49 +0000

    task5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int n
+	int i = 1
+		for (n >= '0' && n <= '9'; n++)
+		{
+			for (n >= '0'; n <= '9'; n++)
+				_putchar(n)
+					if (n >= '9')
+						_putchar(i++)
+							}
+}
