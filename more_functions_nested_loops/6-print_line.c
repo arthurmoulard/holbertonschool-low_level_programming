b1c6a7473548fc17b4a7c04d761de55130588db2
arthurmoulard <arthurmoulardperso@gmail.com>
2025-10-28 12:38:08 +0000

    task6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
- * followed by a new line
- */
-void more_numbers(void)
-{
-	int i, j;
+* print_line- ecris des lignes pour chaque unite
+* @n: tres compliquer la task
+* Return: Always 0 (Success)
+*/
 
-	for (i = 0; i < 10; i++)
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
 	{
-		for (j = 0; j < 15; j++)
+	_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			if (j >= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+		_putchar('_');
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
