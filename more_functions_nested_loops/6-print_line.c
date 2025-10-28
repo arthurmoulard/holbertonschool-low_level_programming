feb4087c87ef836486243676996fa6a9f9b68598
arthurmoulard <arthurmoulardperso@gmail.com>
2025-10-28 10:15:39 +0000

    task6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,13 +2,17 @@
 
 void print_line(int n)
 
-int i = '_'
 {
-	if (n > '0')
-	{
-		_putchar(i * n);
-	        _putchar('\n');
-	}
-	else
+	if (n <= '0')
 		_putchar('\n');
+
+	else
+	{
+		int i;
+			for(i = 0; i < n; i++)
+			{
+				_putchar('_');
+			}
+			_putchar('\n');
+	}
 }
