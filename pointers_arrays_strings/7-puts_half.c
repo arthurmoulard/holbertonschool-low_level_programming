b6eddd6ff42a24695b8bcb01b75f7f5263362ec3
arthurmoulard <arthurmoulardperso@gmail.com>
2025-11-02 16:25:25 +0000

    task7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 5; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
