d32fe45b6f197b6acc0e341fa7482bbac218813a
arthurmoulard <arthurmoulardperso@gmail.com>
2025-11-03 12:55:09 +0000

    task0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,13 @@
-#include 'main.h'
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
@@ -10,5 +17,5 @@ char *_memset(char *s, char b, unsigned int n)
 		s[i] = b;
 	}
 
-	return(s);
+	return (s);
 }
