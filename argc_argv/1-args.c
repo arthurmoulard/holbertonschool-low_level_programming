6d610ecdec8cdd5410657dde4d781f0c1a61965e
arthurmoulard <arthurmoulardperso@gmail.com>
2025-11-05 14:31:12 +0000

    task1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - print the number of arguments passed to the program
@@ -11,5 +11,5 @@
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
-		return(0);
+	return (0);
 }
