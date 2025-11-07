5b593808c9f39c43fa30e5b57fed7a47673ec932
arthurmoulard <arthurmoulardperso@gmail.com>
2025-11-07 13:12:17 +0000

    task4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+int _pow_recursion(int x, int y)
+{
+	if(y < 0)
+	{
+		return(-1);
+	}
+	else if(y == 0)
+	{
+		return(0);
+	}
+	else
+	{
+		return(x * _pow_recursion(x, y - 1));
+	}
+}
