bb117e50ef522f4a50a41d78e71d3116d2a4721e
arthurmoulard <arthurmoulardperso@gmail.com>
2025-11-19 09:37:49 +0000

    task0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -6,9 +6,9 @@
  * @name: name to print
  * @f: pointer to the printing function
  */
- void print_name(char *name, void (*f)(char *))
- {
-	 if (name == NULL || f == NULL)
- 		return;
- 	f(name);
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
 }
