631172e2bb83589c61cde936fb0d76efc3751873
arthurmoulard <arthurmoulardperso@gmail.com>
2025-11-19 09:29:03 +0000

    task2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,5 +1,4 @@
-#include "function_pointer.h"
-#include <stdlib.h>
+#include "function_pointers.h"
 
 /**
  * int_index - earches for an integer
@@ -13,20 +12,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if (cmp == NULL || array == NULL)
-		return (-1);
-
-	if (size <= 0)
-		return (-1);
-
-	for (; i < size ; i++)
-		if (cmp(array[i]))
-			return (i);
-
-	if (i == size)
-		return (-1);
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
 
 	return (-1);
 }
