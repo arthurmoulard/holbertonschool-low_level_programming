19e32371bf6ddbb1ae9a865e7636bd9973237199
arthurmoulard <arthurmoulardperso@gmail.com>
2025-12-05 13:59:14 +0000

    task 0 project2

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	if(h == NULL)
+		return(count);
+
+	while(h->prev != NULL)
+		h = h->prev;
+
+	while(h != NULL)
+	{
+		printf("%d\n", h->next)
+			count++;
+		h = h->next:
+	}
+	return(count)
+}
