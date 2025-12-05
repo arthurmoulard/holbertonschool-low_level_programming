a802353ae33680cfac88421e9f631922f84193b4
arthurmoulard <arthurmoulardperso@gmail.com>
2025-12-05 14:30:16 +0000

    task0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -12,9 +12,9 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while(h != NULL)
 	{
-		printf("%d\n", h->next)
+		printf("%d\n", h->n);
 			count++;
-		h = h->next:
+		h = h->next;
 	}
-	return(count)
+	return(count);
 }
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,7 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -9,7 +12,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
@@ -27,4 +30,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
 #endif
